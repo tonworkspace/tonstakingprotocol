229505d1928af34e8769b908b0e96806e64dbd44
tonworkspace <tonstake591@gmail.com>
2025-01-24 07:51:33 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ton-stake-it',
+  base: '/tonstakingprotocol',
   plugins: [
     react(),
     tsconfigPaths(),
